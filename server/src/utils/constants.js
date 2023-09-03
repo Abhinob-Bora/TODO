d4044832cadcb20b4ee65313ab425e6d1adf3dfe
Abhinob-Bora <abhinobbora6@gmail.com>
2023-09-04 00:29:37 +0530

    commited

---
+++ server/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const DB_CONNECT ="mongodb+srv://TODO:mithoi1@todo.cjrgt4r.mongodb.net/todo?retryWrites=true&w=majority"
+
+export const JWT_TOKEN_SECRET="skkdkjhdkih"
+
+export const StatusCode={
+    SUCCESS:200,
+    VALIDATION_ERROR:201,
+    UNPROCESSABLE_ENTITY:202,
+}
